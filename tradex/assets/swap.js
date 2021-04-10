6f0973811be851dc3a43235efcbce5bd1f41c2ee
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2021-04-10 19:01:43 +0700

    Update swap.js

--- tradex/assets/swap.js
+++ tradex/assets/swap.js
@@ -5,7 +5,7 @@ const app = new Vue({
       prices: null,
       results: {},
       tokens: ["bgov", "pancaketools", "vancat"],
-      amounts: [ 65.03, 50.67042621, 7200130 ]
+      amounts: [ 65.03, 50.67042621, 43200400 ]
     }
   },
   methods: {
