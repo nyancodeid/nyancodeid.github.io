bbf640a8142715526fe774bd5b30ab41880a6bc2
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2021-04-10 14:53:11 +0700

    Add swap javascript

---
+++ tradex/assets/swap.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+  el: "#app",
+  data () {
+    return {
+      prices: null,
+      results: {},
+      tokens: ["bgov", "pancaketools"],
+      amounts: [ 62.90504567, 50.67042621 ]
+    }
+  },
+  methods: {
+    fetchPrices () {
+      return fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${this.tokens.join(',')}&vs_currencies=idr&include_24hr_change=true`).then(res => res.json())
+    }
+  },
+  async mounted () {
+    let total = 0;
+
+    const prices = await this.fetchPrices();
+
+    this.prices = prices;
+    this.tokens.forEach((token, index) => {
+      const price = prices[token]["idr"];
+      const totalPrice = this.amounts[index] * price;
+
+      this.results[token] = { idr: price, total: "Rp. " + (totalPrice).toFixed(0) }
+
+      total += totalPrice;
+    })
+
+    this.results.total = "Rp. " + total.toFixed(0)
+  }
+})
